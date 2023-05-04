6904025c0058739e02d1ecdf8a2dcdc905e96d2d
typeperfest <perfect2111@gmail.com>
2023-05-04 22:19:27 +0300

    add first test

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,7 +3,11 @@
 #include <vector>
 
 TEST(TEST01, BasicTesting) {
-    std::vector<std::vector<int>> v{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
-    IntMatrix mat1(v);
-    
+    std::vector<std::vector<int>> v1{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+    std::vector<std::vector<int>> v2{{3, 2, 1}, {3, 2, 1}, {3, 2, 1}};
+    std::vector<std::vector<int>> vr{{4, 4, 4}, {4, 4, 4}, {4, 4, 4}};
+    IntMatrix mat1(v1);
+    IntMatrix mat2(v2);
+    IntMatrix matr(vr);
+    EXPECT_EQ(mat1 + mat2, matr);
 };
