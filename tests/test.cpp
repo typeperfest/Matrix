6cc03038dc8fc7e15cc2f2862a4da7e799159198
typeperfest <perfect2111@gmail.com>
2023-05-05 15:35:30 +0300

    test implementation

--- tests/test.cpp
+++ tests/test.cpp
@@ -39,6 +39,25 @@ TEST(TEST02, BasicConstruction) {
     IntMatrix mat3(std::move(mat2));
     vvecint v4{{}};
     IntMatrix mat4(v3);
+
+    IntMatrix numbersConstructed1(7, 7);
+    IntMatrix zeros1({{0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0}});
+    EXPECT_EQ(numbersConstructed1, zeros1);
+    IntMatrix numbersConstructed2(1, 1);
+    IntMatrix zeros2({{0}});
+    EXPECT_EQ(numbersConstructed2, zeros2);
+    IntMatrix numbersConstructed3(2, 4);
+    IntMatrix zeros3({{0, 0, 0, 0}, {0, 0, 0, 0}});
+    EXPECT_EQ(numbersConstructed3, zeros3);
+    IntMatrix numbersConstructed4(4, 2);
+    IntMatrix zeros4({{0, 0}, {0, 0}, {0, 0}, {0, 0}});
+    EXPECT_EQ(numbersConstructed4, zeros4);
 }
 
 TEST(TEST03, BasicAssignation) {
@@ -46,14 +65,48 @@ TEST(TEST03, BasicAssignation) {
     IntMatrix m1(v1);
     IntMatrix m2;
     m2 = m1;
+    EXPECT_EQ(m1, m2);
     IntMatrix v2{{1, 2, 3}, {1, 2, 3}};
     IntMatrix m3(v2);
     m3 = m2;
+    EXPECT_EQ(m2, m3);
     IntMatrix m4;
     m4 = std::move(m3);
+    EXPECT_EQ(m4, m2);
 }
 
-TEST(TEST04, ArithmeticOperations) {
+TEST(TEST04, CompatibilityChecking) {
+    IntMatrix mat1({{1, 2, 3, 4, 5}});
+    IntMatrix mat2({{1}, {2}, {3}, {4}, {5}});
+    IntMatrix mat3({{1}});
+    IntMatrix squaredMat1({{1, 2, 3}, {1, 2, 3}, {1, 2, 3}});
+    IntMatrix squaredMat2({{1, 2, 3}, {1, 2, 3}, {1, 2, 3}});
+    IntMatrix mat6({{1, 2, 3, 4, 5}});
+    IntMatrix empMat({{}});
+
+    EXPECT_TRUE(squaredMat1.isCompatible(squaredMat2, Operation::ADDITION));
+    EXPECT_TRUE(squaredMat1.isCompatible(squaredMat2, Operation::SUBSTRACTION));
+    EXPECT_TRUE(squaredMat1.isCompatible(squaredMat2, Operation::MULTIPLICATION));
+    EXPECT_TRUE(squaredMat1.isCompatible(squaredMat2, Operation::COMPARISON));
+
+    EXPECT_TRUE(mat1.isCompatible(mat2, Operation::MULTIPLICATION));
+    EXPECT_TRUE(mat2.isCompatible(mat1, Operation::MULTIPLICATION));
+    EXPECT_FALSE(mat1.isCompatible(mat2, Operation::ADDITION));
+    EXPECT_FALSE(mat1.isCompatible(mat2, Operation::SUBSTRACTION));
+    EXPECT_FALSE(mat1.isCompatible(mat2, Operation::COMPARISON));
+
+    EXPECT_TRUE(mat1.isCompatible(mat6, Operation::ADDITION));
+    EXPECT_TRUE(mat1.isCompatible(mat6, Operation::SUBSTRACTION));
+    EXPECT_TRUE(mat1.isCompatible(mat6, Operation::COMPARISON));
+    EXPECT_FALSE(mat1.isCompatible(mat6, Operation::MULTIPLICATION));
+
+    EXPECT_FALSE(empMat.isCompatible(mat1, Operation::ADDITION));
+    EXPECT_FALSE(empMat.isCompatible(mat1, Operation::MULTIPLICATION));
+    EXPECT_FALSE(empMat.isCompatible(mat1, Operation::SUBSTRACTION));
+    EXPECT_FALSE(empMat.isCompatible(mat1, Operation::COMPARISON));
+}
+
+TEST(TEST05, ArithmeticOperations) {
     vvecint v1{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
     vvecint v2{{3, 2, 1}, {3, 2, 1}, {3, 2, 1}};
     vvecint vr{{4, 4, 4}, {4, 4, 4}, {4, 4, 4}};
@@ -61,7 +114,15 @@ TEST(TEST04, ArithmeticOperations) {
     IntMatrix mat2(v2);
     IntMatrix matr(vr);
     EXPECT_EQ(mat1 + mat2, matr);
-    // TODO: implement compatibility checking
-    // TODO: implement substraction
-    // TODO: implement multiplication
+    IntMatrix rowMat1({{7, 7, 7}});
+    IntMatrix rowMat2({{3, 3, 3}});
+    IntMatrix rowMat3({{4, 4, 4}});
+    IntMatrix rowMat5 = rowMat1 - rowMat2; 
+    EXPECT_EQ(rowMat5, rowMat3);
+    IntMatrix rowMat4({{5, 5, 5}});
+    IntMatrix colMat1({{4}, {4}, {4}});
+    IntMatrix singleMat1({{60}});
+    IntMatrix colMat2({{20, 20, 20}, {20, 20, 20}, {20, 20, 20}});
+    EXPECT_EQ(rowMat4 * colMat1, singleMat1);
+    EXPECT_EQ(colMat1 * rowMat4, colMat2);
 };
