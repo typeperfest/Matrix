32ed2471721429b4e6b0e51bc30ebe0ac9e88480
typeperfest <perfect2111@gmail.com>
2023-05-17 23:52:50 +0300

    add definition check

--- benchmark/main_SSE.cpp
+++ benchmark/main_SSE.cpp
@@ -2,6 +2,10 @@
 #include "../lib/IntMatrix.h"
 
 int main(int argc, char* argv[]) {
-    
+#ifdef SIMD_EXTENSION_ENABLED
+    std::cout << "SIMD_EXTESION_ENABLED is defined" << std::endl;
+#else
+    std::cout << "SIMD_EXTESION_ENABLED is undefined" << std::endl;
+#endif
     return 0;
 }
