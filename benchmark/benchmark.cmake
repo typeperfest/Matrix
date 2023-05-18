1e6f6a419d9fb2fb60feca8358d2f3c2a92bf6a0
typeperfest <perfect2111@gmail.com>
2023-05-18 15:57:40 +0300

    disappointment

--- benchmark/benchmark.cmake
+++ benchmark/benchmark.cmake
@@ -6,13 +6,13 @@ project(IntMatrixBench)
 
 # With custom SSE, with O3
 add_executable(benchSSE_O3 ${CMAKE_CURRENT_SOURCE_DIR}/benchmark/main.cpp)
-target_compile_options(benchSSE_O3 PRIVATE "-O3")
+target_compile_options(IntMatrix PRIVATE "-O3")
 target_compile_definitions(benchSSE_O3 PRIVATE "O3_ENABLED")
 target_link_libraries(benchSSE_O3 IntMatrixSSE)
 
 # Without custom SSE, with O3
 add_executable(benchO3 ${CMAKE_CURRENT_SOURCE_DIR}/benchmark/main.cpp)
-target_compile_options(benchO3 PRIVATE "-O3")
+target_compile_options(IntMatrix PRIVATE "-O3")
 target_compile_definitions(benchO3 PRIVATE "O3_ENABLED")
 target_link_libraries(benchO3 IntMatrix)
 
